bc0c3673ff6271c5ce837dff3d951a73adf17f83
maumita14 <senmaumita302@gmail.com>
2025-11-12 19:23:19 +0530

    Create _merge_array.c

---
+++ _merge_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int size1;
+	
+	//1st array
+	print("Enter the size of 1st Array->");
+	scanf("%d",&size1);
+	int arr1[size1];
+	for(int i=0;i<size1;i++){
+		printf("Enter number%d->",i+1);
+		scanf("%d",&arr1[i]);
+}
+	
+	
+	
+	// merge 
+	int f_size=size1+size2;
+	intf_array[f_size];
+	//merge 1st array element in new array
+	for(int i=0;i<size1;i++){
+		f_array[i]=arr1[i];
+		
+		
+		
+	}
+	return 0;
+}
